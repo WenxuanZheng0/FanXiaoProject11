153634ae1b65df7f6862b4ed45597c760db4a0de
WenxuanZheng0 <97931921+WenxuanZheng0@users.noreply.github.com>
2022-01-20 21:53:57 +0800

    initialize

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///
+///</summary>
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] protected string selectableTag = "Objects";
+    //[SerializeField] private Material HighLightMaterial;
+    //[SerializeField] private Material defaultMaterial;
+
+
+    [SerializeField] private GameObject pickUpCanvas;
+    private Transform selectionTrans;
+
+    protected bool isIntrigger = false;
+    private void Start()
+    {
+        pickUpCanvas.SetActive(false);   
+    }
+    private void Update()
+    {
+        float distance = 10000;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (selectionTrans != null)
+        {
+            //var selectionRenderer = selectionTrans.GetComponent<Renderer>();
+            //var selectionCanvas = selectionTrans.GetComponentInChildren<Canvas>();
+            //selectionCanvas.enabled = false;
+            //selectionRenderer.material = defaultMaterial;
+
+            pickUpCanvas.SetActive(false);
+            selectionTrans = null;
+
+        }
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            distance = (transform.position - selection.position).sqrMagnitude;
+            if (selection.CompareTag(selectableTag) && distance < 4)
+            {
+                //var selectionRender = selection.GetComponent<Renderer>();
+                //var selectionCanvas = selection.GetComponentInChildren<Canvas>();
+                //if (selectionRender != null)
+                //{
+                //    //selectionCanvas.enabled = true;
+                //    //selectionRender.material = HighLightMaterial;
+                //}
+
+                pickUpCanvas.SetActive(true);
+                selectionTrans = selection;
+            }
+        }
+    }
+}
