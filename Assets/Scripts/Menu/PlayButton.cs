b22861510b08077dd0e4af0ac2f622cf0c7602c0
WanianXO <752470626@qq.com>
2022-01-22 00:52:06 +0800

    主菜单

---
+++ Assets/Scripts/Menu/PlayButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+///<summary>
+///
+///</summary>
+public class PlayButton : MonoBehaviour
+{
+    public GameObject loading;
+    private void Awake()
+    {
+        loading.SetActive(false);
+    }
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //当前场景的下一个场景
+        transform.gameObject.SetActive(false);
+        loading.SetActive(true);
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
