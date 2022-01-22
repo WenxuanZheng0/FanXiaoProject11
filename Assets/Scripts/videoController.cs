8015d0e1e0ffda980be47cc4922099ae90887799
WanianXO <752470626@qq.com>
2022-01-22 09:59:43 +0800

    结束了

---
+++ Assets/Scripts/videoController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+///<summary>
+///
+///</summary>
+public class videoController : MonoBehaviour
+{
+    public VideoPlayer m_VideoPlayer;
+    [SerializeField] private GameObject loading;
+    [SerializeField] private GameObject VideoScreen;
+
+    void Awake()
+    {
+        m_VideoPlayer = GetComponent<VideoPlayer>();
+        m_VideoPlayer.loopPointReached += OnMovieFinished; // loopPointReached is the event for the end of the video
+        loading.SetActive(false);
+        VideoScreen.SetActive(true);
+    }
+
+    void OnMovieFinished(VideoPlayer player)
+    {
+        Debug.Log("Event for movie end called");
+        loading.SetActive(true);
+        player.Stop();
+        VideoScreen.SetActive(false);
+        SceneManager.LoadScene(2);
+    }
+}
